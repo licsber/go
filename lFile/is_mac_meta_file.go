b0540bb49f302ac6ac5cd97e401b48ec9d29d0fc
licsber <admin@licsber.site>
2023-01-09 13:13:58 +0800

    add 新增mac元文件的判断.

--- lFile/is_mac_meta_file.go
+++ lFile/is_mac_meta_file.go
@@ -5,12 +5,18 @@ import (
 	"strings"
 )
 
-const macMetaStr = "This resource fork intentionally left blank"
+const macMetaStr1 = "com.apple.quarantine"
+const macMetaStr2 = "This resource fork intentionally left blank"
 
 func IsMacMetaFile(p string) bool {
 	b, err := os.ReadFile(p)
 	if err != nil {
 		return false
 	}
-	return strings.Contains(string(b), macMetaStr)
+
+	if strings.Contains(string(b), macMetaStr1) {
+		return true
+	}
+
+	return strings.Contains(string(b), macMetaStr2)
 }
