63d719f7a4142ea5452e7cb5e1ba7d87819c95da
licsber <admin@licsber.site>
2023-01-22 12:37:01 +0800

    add 修改S3部分默认值.

--- kit/lS3/config.go
+++ kit/lS3/config.go
@@ -11,28 +11,31 @@ type Config struct {
 }
 
 func (c *Config) Must() {
-	if c.Endpoint != "" && c.Region != "" && c.AccessKey != "" && c.SecretKey != "" {
-		return
-	}
-
-	c.Endpoint = os.Getenv("L_S3_ENDPOINT")
-	c.Region = os.Getenv("L_S3_REGION")
-	c.AccessKey = os.Getenv("L_S3_ACCESS")
-	c.SecretKey = os.Getenv("L_S3_SECRET")
-
 	if c.Endpoint == "" {
-		panic("lS3 need L_S3_ENDPOINT env var.")
+		c.Endpoint = os.Getenv("L_S3_ENDPOINT")
+		if c.Endpoint == "" {
+			panic("lS3 need L_S3_ENDPOINT env var.")
+		}
 	}
 
 	if c.Region == "" {
-		panic("lS3 need L_S3_REGION env var.")
+		c.Region = os.Getenv("L_S3_REGION")
+		if c.Region == "" {
+			panic("lS3 need L_S3_REGION env var.")
+		}
 	}
 
 	if c.AccessKey == "" {
-		panic("lS3 need L_S3_ACCESS env var.")
+		c.AccessKey = os.Getenv("L_S3_ACCESS")
+		if c.AccessKey == "" {
+			panic("lS3 need L_S3_ACCESS env var.")
+		}
 	}
 
 	if c.SecretKey == "" {
-		panic("lS3 need L_S3_SECRET env var.")
+		c.SecretKey = os.Getenv("L_S3_SECRET")
+		if c.SecretKey == "" {
+			panic("lS3 need L_S3_SECRET env var.")
+		}
 	}
 }
