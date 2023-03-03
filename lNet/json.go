1382d8cde390c8d243c4e1400dc7dc8980cf8742
licsber <admin@licsber.site>
2023-03-03 15:30:49 +0800

    add lNet的JSON修改为公有方法.

--- lNet/json.go
+++ lNet/json.go
@@ -2,7 +2,7 @@ package lNet
 
 import "encoding/json"
 
-func (c *Client) JSON(p []byte, err error) (map[string]interface{}, error) {
+func JSON(p []byte, err error) (map[string]interface{}, error) {
 	if err != nil {
 		return nil, err
 	}
