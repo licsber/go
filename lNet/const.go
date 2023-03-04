4c8358d9f3b80c7f9a037869414e15570a5d0ecb
licsber <admin@licsber.site>
2023-03-04 10:14:17 +0800

    add 增加错误代码.

--- lNet/const.go
+++ lNet/const.go
@@ -2,8 +2,10 @@ package lNet
 
 import "errors"
 
+var ErrBadRequest = errors.New("lNet: 400 Bad Request")
 var ErrUnAuthorized = errors.New("lNet: 401 UnAuthorized")
 var ErrForbidden = errors.New("lNet: 403 Forbidden")
 var ErrNotFound = errors.New("lNet: 404 Not Found")
+var ErrInternalServerError = errors.New("lNet: 500 Internal Server Error")
 var ErrBadGateway = errors.New("lNet: 502 Bad Gateway")
 var ErrUnknown = errors.New("lNet: Unknown error")
