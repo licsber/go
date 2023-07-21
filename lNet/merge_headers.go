b44d0403682a13be57bbfd1ca2069fe0c0fa3c62
licsber <admin@licsber.site>
2023-07-21 15:29:06 +0800

    fix merge headers.

--- lNet/merge_headers.go
+++ lNet/merge_headers.go
@@ -1,10 +1,11 @@
 package lNet
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func MergeHeaders(a, b http.Header) {
-	for k := range b {
-		v := b.Get(k)
-		a.Set(k, v)
+	for k, v := range b {
+		a[k] = v
 	}
 }
